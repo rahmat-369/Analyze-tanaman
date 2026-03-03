a3d9660ee69589a8a0c5f8f4bfd7ebc66f00c088
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-03-03 08:28:57 +0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -98,4 +98,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ error: "Server Error", detail: error.message });
     }
-                     } 
+            }
